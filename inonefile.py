9722435e607ca7f0cb875a81df8f7b96e5801150
Fedoseeva <mfedosseyeva@gmail.com>
2016-11-25 14:25:49 +0100

    ex2 - 8

--- inonefile.py
+++ inonefile.py
@@ -6,6 +6,8 @@ import time
 import urllib.request
 import xml.etree.ElementTree as EL
 
+display_size = [16, 2]
+
 # you can ignore these three lines of code
 # they are needed so that you can end the
 # program by pressing Ctrl+C
@@ -16,7 +18,11 @@ signal.signal(signal.SIGINT, signal_handler)
 def chunkstring(string, length):
     return (string[0+i:length+i] for i in range(0, len(string), length))
 
-display_size = [16, 2]
+def show_greeting(greeting, sroll_speed):
+	for chunk in chunkstring(greeting[3].text, display_size[0] * display_size[1]):
+		cad.lcd.write('\n'.join(chunkstring(chunk, display_size[0])
+		time.sleep(scroll_speed)
+
 # fetch the data
 xml = urllib.request.urlopen("http://192.168.1.100:8080/rest/guestbook/").read()
 # parse the data and create a XML tree
@@ -26,15 +32,32 @@ root = EL.fromstring(xml)
 # the content tag is the 4th (so index 3)
 # child of a greeting
 
-all_greetings = ""
-for greeting in root:
-	all_greetings += greeting[3].text
+# all_greetings = ""
+# for greeting in root:
+# 	all_greetings += greeting[3].text
+# 
+# cad = pifacecad.PiFaceCAD()
+# cad.lcd.write(all_greetings)
+# 
+# for greeting in root:
+# 	for chunk in chunkstring(greeting, display_size[0] * display_size[1]):
+# 		cad.lcd.write('\n'.join(chunkstring(chunk, display_size[0])
+# 		time.sleep(1)
 
-cad = pifacecad.PiFaceCAD()
-cad.lcd.write(all_greetings)
+guestbook_len = len(root)
+current_greeting = 0
+show_greeting(root[current_greeting], 1)
 
-for greeting in root:
-	for chunk in chunkstring(greeting, display_size[0] * display_size[1]):
-		cad.lcd.write('\n'.join(chunkstring(chunk, display_size[0])
-		time.sleep(1)
+def update_pin_text(event):
+	event.chip.lcd.set_cursor(0, 0)
+	event.chip.lcd.clear()
+	print(str(event.pin_num))
+	if event.pin_num == 4:
+		current_greeting = (current_greeting + 1) % guestbook_len
+		show_greeting(root[current_greeting], 1)
 
+listener = pifacecad.SwitchEventListener(chip=cad)
+
+for i in range(8):
+	listener.register(i, pifacecad.IODIR_FALLING_EDGE, update_pin_text)
+listener.activate()
